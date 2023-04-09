2e738e57138220b3d8a201dd1906ae549c179a6d
daguang123456 <poiuylkjhgmnbvc12@outlook.com>
2023-04-09 14:29:12 -0700

    update

--- app.py
+++ app.py
@@ -20,9 +20,51 @@ authenticator = stauth.Authenticate(
 name, authentication_status, username = authenticator.login('Login', 'main')
 if authentication_status:
     authenticator.logout('Logout', 'main')
-    page = st.sidebar.selectbox("探索", ("历史上的今天","历史上","小数学事实","随机事实","年事实"))
+    page = st.sidebar.selectbox("探索", ("QUOTE","历史上的今天","历史上","小数学事实","随机事实","年事实"))
 
-    if page == "历史上的今天":
+    if page == "QUOTE":
+        st.title("名人名言")
+        url = "https://quotel-quotes.p.rapidapi.com/search/quotes"
+        pagen = st.slider("页",1,10000,1,1)
+        numperpage = st.slider("每页结果数",1,30,1,1)
+        prompt = st.text_input("提示", value="")
+        if st.button("生成"):
+            with st.spinner("请稍候..."):
+
+                payload = {
+                    "pageSize": numperpage,
+                    "page": pagen,
+                    "searchString": prompt
+                }
+                headers = {
+                    "content-type": "application/json",
+                    "X-RapidAPI-Key": "bd10c657b4msh6dbd4f9bf219b22p14f68ajsn0d507d4fae87",
+                    "X-RapidAPI-Host": "quotel-quotes.p.rapidapi.com"
+                }
+
+                response = requests.request("POST", url, json=payload, headers=headers)
+                ini_string_s = response.json()
+                for dictionary in ini_string_s:
+                    try:
+                        st.text(str(dictionary["quoteId"]))
+                        # for key in dictionary:
+                            # print(dictionary[key])
+                            # st.text(str(key)+": "+str(dictionary[key]))
+
+                        st.text("引用: "+dictionary["quote"])
+                        st.text("国籍: "+dictionary["nationality"])
+                        st.text("职业: "+dictionary["profession"])
+                        st.text("出生: "+dictionary["born"])
+                        st.text("逝世: "+dictionary["died"])
+                        st.text("名字: "+dictionary["name"])
+                        st.text(" ")
+                    except:
+                        pass
+
+
+                # print(response.text)
+
+    elif page == "历史上的今天":
         st.title("历史上的今天")
         url = "https://today-in-history.p.rapidapi.com/thisday"
 
